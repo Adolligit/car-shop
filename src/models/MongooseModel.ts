42368d31292fdc9c1b725715559b3cd2764b08f5
Adolligit <adelson.onod@gmail.com>
2022-11-06 20:05:12 -0300

    feat: (MongooseModel) Model com mongoose criada

---
+++ src/models/MongooseModel.ts
@@ -0,0 +1,18 @@
+import { Schema, model as modelMongoose, Model } from 'mongoose';
+import { IModel } from '../interfaces/IModel';
+
+abstract class MongooseModel<T> implements IModel<T> {
+  protected _model: Model<T>;
+
+  constructor(nameModel: string) {
+    this._model = modelMongoose(nameModel, new Schema<T>());
+  }
+  
+  public create = async (obj: T): Promise<T> => this._model.create({ ...obj });
+  public read = async (): Promise<T[]> => this._model.find();
+  public readOne = async (_id: string): Promise<T | null> => this._model.findOne({ _id });
+  public update = async (id: string, obj: T): Promise<T | null> => this._model.findOneAndUpdate({ id }, { ...obj });
+  public delete = async (id: string): Promise<T | null> => this._model.findOneAndDelete({ id });
+}
+
+export default MongooseModel;
