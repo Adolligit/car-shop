8bb695d989c6d5ca39ede599e19b9c0c432b9895
Adolligit <adelson.onod@gmail.com>
2022-11-06 20:19:29 -0300

    fix: erro local com o retorno da função 'update'

--- src/models/MongooseModel.ts
+++ src/models/MongooseModel.ts
@@ -1,4 +1,4 @@
-import { Schema, model as modelMongoose, Model } from 'mongoose';
+import { Schema, model as modelMongoose, Model, UpdateQuery } from 'mongoose';
 import { IModel } from '../interfaces/IModel';
 
 abstract class MongooseModel<T> implements IModel<T> {
@@ -11,8 +11,9 @@ abstract class MongooseModel<T> implements IModel<T> {
   public create = async (obj: T): Promise<T> => this._model.create({ ...obj });
   public read = async (): Promise<T[]> => this._model.find();
   public readOne = async (_id: string): Promise<T | null> => this._model.findOne({ _id });
-  public update = async (id: string, obj: T): Promise<T | null> => this
-    ._model.findOneAndUpdate({ id }, { ...obj });
+  public update = async (id: string, obj: T): Promise<T | null> => (
+    this._model.findOneAndUpdate({ id }, { ...obj } as UpdateQuery<T>)
+  );
   public delete = async (id: string): Promise<T | null> => this._model.findOneAndDelete({ id });
 }
 
