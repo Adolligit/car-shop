6d4eac111b3a645d105cf166f03ceb39433d6450
Adolligit <adelson.onod@gmail.com>
2022-11-09 06:52:34 -0300

    test: primeiro it

---
+++ src/tests/mocks/CarModel.ts
@@ -0,0 +1,11 @@
+const create = {
+  "_id": "636b6c1bd7bdda04cbb9a40e",
+  "model": "BMW I8 Roadster",
+  "year": 2020,
+  "color": "orange",
+  "buyValue": 3600000,
+  "seatsQty": 2,
+  "doorsQty": 2
+}
+
+export default { create };
--- src/tests/unit/models/CarModel.test.ts
+++ src/tests/unit/models/CarModel.test.ts
@@ -2,15 +2,27 @@ import * as sinon from 'sinon';
 import chai from 'chai';
 const { expect } = chai;
 
+import mocks from '../../mocks/CarModel';
+import { Model } from 'mongoose';
+import CarModel from '../../../models/car.model';
+import { ICar } from '../../../interfaces/ICar';
+
 // before after= executa uma vez antes de todos os testes
 // beforeAll afterAll= executa uma vez antes de cada testes
 
 describe('(Car: Model)', () => {
+  const carModel = new CarModel('Car');
 
   after(() => sinon.restore());
 
-  describe('{ function: create }', () => {
-    before(() => sinon.stub().resolves());
+  describe('{ function: create }: implementando teste de vento', () => {
+    before(() => sinon.stub(Model, 'create').resolves(mocks.create));
+
+    it('vai tudo certinho, vai vendo...', async () => {
+      const created = await carModel.create(mocks.create);
+
+      expect(created).to.be.deep.equal(mocks.create);
+    });
   });
 
 });
