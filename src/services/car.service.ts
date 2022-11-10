de30587a050cb1b1aaf5b28ef6789dc1d66e3e2f
Adolligit <adelson.onod@gmail.com>
2022-11-09 22:14:54 -0300

    refactor: (Car: Service) ao invés de criar um objeto de 'CarModel', agora deixa essa responsabilidade para cria com com esta classe

--- src/services/car.service.ts
+++ src/services/car.service.ts
@@ -1,38 +1,40 @@
 import { ICar } from '../interfaces/ICar';
-import Car from '../models/car.model';
+import CarModel from '../models/car.model';
 
 class CarService {
-  private static _carModel: Car = new Car('Car');
+  private _carModel: CarModel;
 
-  public static async create(obj: ICar) {
+  constructor(carModel: CarModel) { this._carModel = carModel; }
+
+  public async create(obj: ICar) {
     const created = await this._carModel.create(obj);
 
-    return { _id: created._id, ...obj } as ICar;
+    return created as ICar;
   }
 
-  public static async read() {
-    const wasRead = await this._carModel.read();
+  // public async read() {
+  //   const wasRead = await this._carModel.read();
 
-    return wasRead;
-  }
+  //   return wasRead;
+  // }
 
-  public static async readOne(id: string) {
-    const wasFound = await this._carModel.readOne(id);
+  // public async readOne(id: string) {
+  //   const wasFound = await this._carModel.readOne(id);
 
-    return wasFound;
-  }
+  //   return wasFound;
+  // }
 
-  public static async update(id: string, payload: ICar) {
-    const updated = await this._carModel.update(id, payload);
+  // public async update(id: string, payload: ICar) {
+  //   const updated = await this._carModel.update(id, payload);
 
-    return updated;
-  }
+  //   return updated;
+  // }
 
-  public static async delete(id: string) {
-    const deleted = await this._carModel.delete(id);
+  // public async delete(id: string) {
+  //   const deleted = await this._carModel.delete(id);
 
-    return deleted;
-  }
+  //   return deleted;
+  // }
 }
 
 export default CarService;
