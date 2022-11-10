a80fb0ea771d6d808857d46d53c0ac93edcebf46
Adolligit <adelson.onod@gmail.com>
2022-11-09 22:15:39 -0300

    refactor: (Car: Controller) inversão de dependência

--- src/controllers/car.controlller.ts
+++ src/controllers/car.controlller.ts
@@ -2,8 +2,12 @@ import { Request, Response } from 'express';
 import CarService from '../services/car.service';
 
 class CarController {
-  public static async create(req: Request, res: Response) {
-    return res.status(201).json(await CarService.create(req.body));
+  private _carService: CarService;
+
+  constructor(carService: CarService) { this._carService = carService; }
+
+  public async create(req: Request, res: Response) {
+    return res.status(201).json(await this._carService.create(req.body));
   }
 }
 
