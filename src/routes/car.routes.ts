177be01b533eaeca3fd6fc235ae583d4a35d3a5f
Adolligit <adelson.onod@gmail.com>
2022-11-09 22:17:22 -0300

    refactor: (Car: Routes) agora objetos de 'model, 'service' e 'controller' são instânciados

--- src/routes/car.routes.ts
+++ src/routes/car.routes.ts
@@ -1,11 +1,21 @@
 import express from 'express';
 import 'express-async-errors';
 
+import CarModel from '../models/car.model';
 import CarController from '../controllers/car.controlller';
-import CarMiddleware from '../middlewares/car.middleware';
+import CarService from '../services/car.service';
+import middleware from '../middlewares/car.middleware';
+
+const model = new CarModel();
+const service = new CarService(model);
+const controller = new CarController(service);
 
 const car = express();
 
-car.post('/', CarMiddleware.inputValidation, CarController.create);
+car.post(
+  '/',
+  (req, res, nxt) => middleware.inputValidation(req, res, nxt), 
+  (req, res) => controller.create(req, res),
+);
 
 export default car;
