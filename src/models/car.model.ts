3559cc428c202af5ced74e74da421d614975f254
Adolligit <adelson.onod@gmail.com>
2022-11-09 22:13:28 -0300

    refactor: (Car: Model) agora possui um 'schema' mongoose que é passado para superclasse

--- src/models/car.model.ts
+++ src/models/car.model.ts
@@ -1,6 +1,21 @@
+import { Schema } from 'mongoose';
 import { ICar } from '../interfaces/ICar';
 import MongooseModel from './odm/mongoose.model';
 
-class Car extends MongooseModel<ICar> {}
+const schema = new Schema<ICar>({
+  model: String,
+  year: Number,
+  color: String,
+  status: Boolean,
+  buyValue: Number,
+  doorsQty: Number,
+  seatsQty: Number,
+}, { versionKey: false });
+
+class Car extends MongooseModel<ICar> {
+  constructor() {
+    super('Car', schema);
+  }
+}
 
 export default Car;
