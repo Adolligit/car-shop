6eda831c35a7956ae916ff8b69cb02f5c9d4dbb9
Adolligit <adelson.onod@gmail.com>
2023-05-09 19:48:40 -0300

    fix: (connection) trocando 'localhost' para '127.0.0.1'

--- src/models/connection.ts
+++ src/models/connection.ts
@@ -1,7 +1,7 @@
 import mongoose from 'mongoose';
 import 'dotenv/config';
 
-const MONGO_DB_URL = 'mongodb://localhost:27017/CarShop';
+const MONGO_DB_URL = 'mongodb://127.0.0.1:27017/CarShop';
 
 const connectToDatabase = (
   mongoDatabaseURI = process.env.MONGO_URI
