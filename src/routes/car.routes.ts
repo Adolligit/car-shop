5ce3011e848917f6f0e6d57d549aa7848b1e9474
Adolligit <adelson.onod@gmail.com>
2023-05-29 16:35:41 -0300

    chore: (routes: car, motor...) organizando verbos/métodos http em um conjunto 'route' do Express

--- src/routes/car.routes.ts
+++ src/routes/car.routes.ts
@@ -12,26 +12,17 @@ const controller = new CarController(service);
 
 const car = express();
 
-car.post(
-  '/',
-  (req, res, nxt) => middleware.inputValidation(req, res, nxt), 
-  (req, res) => controller.create(req, res),
-);
-car.get(
-  '/:id',
-  (req, res, nxt) => middleware.idMongoValidation(req, res, nxt),
-  (req, res) => controller.readOne(req, res),
-);
-car.get('/', (req, res) => controller.read(req, res));
-car.put(
-  '/:id',
-  (req, res, nxt) => middleware.idMongoValidation(req, res, nxt),
-  (req, res) => controller.update(req, res),
-);
-car.delete(
-  '/:id',
-  (req, res, nxt) => middleware.idMongoValidation(req, res, nxt),
-  (req, res) => controller.delete(req, res),
-);
+car.route('/')
+  .get((req, res) => controller.read(req, res))
+  .post(
+    middleware.inputValidation,
+    (req, res) => controller.create(req, res),
+  );
+
+car.route('/:id')
+  .all(middleware.idMongoValidation)
+  .get((req, res) => controller.readOne(req, res))
+  .put((req, res) => controller.update(req, res))
+  .delete((req, res) => controller.delete(req, res));
 
 export default car;
--- src/routes/motorcycle.routes.ts
+++ src/routes/motorcycle.routes.ts
@@ -12,26 +12,17 @@ const controller = new MotorcycleController(service);
 
 const motorcycle = express();
 
-motorcycle.post(
-  '/',
-  (req, res, nxt) => middleware.inputValidation(req, res, nxt), 
-  (req, res) => controller.create(req, res),
-);
-motorcycle.get(
-  '/:id',
-  (req, res, nxt) => middleware.idMongoValidation(req, res, nxt),
-  (req, res) => controller.readOne(req, res),
-);
-motorcycle.get('/', (req, res) => controller.read(req, res));
-motorcycle.put(
-  '/:id',
-  (req, res, nxt) => middleware.idMongoValidation(req, res, nxt),
-  (req, res) => controller.update(req, res),
-);
-motorcycle.delete(
-  '/:id',
-  (req, res, nxt) => middleware.idMongoValidation(req, res, nxt),
-  (req, res) => controller.delete(req, res),
-);
+motorcycle.route('/')
+  .get((req, res) => controller.read(req, res))
+  .post(
+    middleware.inputValidation,
+    (req, res) => controller.create(req, res),
+  );
+
+motorcycle.route('/:id')  
+  .all(middleware.idMongoValidation)
+  .get((req, res) => controller.readOne(req, res))
+  .put((req, res) => controller.update(req, res))
+  .delete((req, res) => controller.delete(req, res));
 
 export default motorcycle;
