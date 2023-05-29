d2b21760d9ba341bd43979f3a22e2efb816f9f65
Adolligit <adelson.onod@gmail.com>
2023-05-29 19:12:52 -0300

    feat: (service) abstraindo o serviço das rotas para evitar duplicidade de código

---
+++ src/abstract/Service.ts
@@ -0,0 +1,54 @@
+import CatchAllMethodsErros from '../errors/CatchAllMethodsErrors';
+import { IModel } from '../interfaces/IModel';
+
+const REFATORA_ESSE_CODIGO = 'Object not found';
+
+abstract class Service<T> implements IModel<T> {
+  private _model: IModel<T>;
+
+  constructor(model: IModel<T>) {
+    this._model = model;
+  }
+  
+  public async create(obj: T): Promise<T> {
+    const created = await this._model.create(obj);
+
+    return created as T;
+  }
+  
+  public async read(): Promise<T[]> {
+    const wasRead = await this._model.read();
+    
+    return wasRead;
+  }
+  
+  public async readOne(id: string): Promise<T | null> {
+    const wasFound = await this._model.readOne(id);
+
+    if (!wasFound) throw new CatchAllMethodsErros(REFATORA_ESSE_CODIGO, 404);
+
+    return wasFound;
+  }
+  
+  public async update(id: string, obj: T): Promise<T | null> {
+    const wasFound = await this._model.readOne(id);
+    
+    if (!wasFound) throw new CatchAllMethodsErros(REFATORA_ESSE_CODIGO, 404);
+    if (!obj) throw new CatchAllMethodsErros(REFATORA_ESSE_CODIGO, 400);
+
+    const updated = await this._model.update(id, { ...obj });
+    
+    return updated;
+  }
+  
+  public async delete(id: string): Promise<T | null> {
+    const wasFound = await this._model.readOne(id);
+    
+    if (!wasFound) throw new CatchAllMethodsErros(REFATORA_ESSE_CODIGO, 404);
+    const deleted = await this._model.delete(id);
+
+    return deleted;
+  }
+}
+
+export default Service;
