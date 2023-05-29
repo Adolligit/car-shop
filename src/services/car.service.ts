c09553b3f958125e95362b2682644f885267020d
Adolligit <adelson.onod@gmail.com>
2023-05-29 20:19:17 -0300

    chore: (Car: service) mudanças não salva sobre a implementação da classe abstrata

--- src/services/car.service.ts
+++ src/services/car.service.ts
@@ -1,4 +1,4 @@
-import Service from '../abstract/Service';
+import Service from '../abstract/service.absctract';
 import { ICar } from '../interfaces/ICar';
 
 class CarService extends Service<ICar> {}
