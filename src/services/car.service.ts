910f0e54a290e7ae87345fc75484b1c10a73320d
Adolligit <adelson.onod@gmail.com>
2023-05-29 20:08:50 -0300

    feat: (Car: service) implementando a classe abstrata de serviço

--- src/services/car.service.ts
+++ src/services/car.service.ts
@@ -1,53 +1,6 @@
-import CatchAllMethodsErros from '../errors/CatchAllMethodsErrors';
+import Service from '../abstract/Service';
 import { ICar } from '../interfaces/ICar';
-import CarModel from '../models/car.model';
 
-const REFATORA_ESSE_CODIGO = 'Object not found';
-
-class CarService {
-  private _carModel: CarModel;
-
-  constructor(carModel: CarModel) { this._carModel = carModel; }
-
-  public async create(obj: ICar) {
-    const created = await this._carModel.create(obj);
-
-    return created as ICar;
-  }
-
-  public async read() {
-    const wasRead = await this._carModel.read();
-    
-    return wasRead;
-  }
-
-  public async readOne(id: string) {
-    const wasFound = await this._carModel.readOne(id);
-
-    if (!wasFound) throw new CatchAllMethodsErros(REFATORA_ESSE_CODIGO, 404);
-
-    return wasFound;
-  }
-
-  public async update(id: string, payload: ICar) {
-    const wasFound = await this._carModel.readOne(id);
-    
-    if (!wasFound) throw new CatchAllMethodsErros(REFATORA_ESSE_CODIGO, 404);
-    if (!payload.model) throw new CatchAllMethodsErros(REFATORA_ESSE_CODIGO, 400);
-
-    const updated = await this._carModel.update(id, { ...payload });
-    
-    return updated;
-  }
-
-  public async delete(id: string) {
-    const wasFound = await this._carModel.readOne(id);
-    
-    if (!wasFound) throw new CatchAllMethodsErros(REFATORA_ESSE_CODIGO, 404);
-    const deleted = await this._carModel.delete(id);
-
-    return deleted;
-  }
-}
+class CarService extends Service<ICar> {}
 
 export default CarService;
